4789230d34767049618328d180c509e886798d97
mallek <travis@haleycomputersolutions.com>
2017-02-17 00:11:38 -0700

    added mongoose queries to playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,45 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/modles/todo');
+const {User} = require('./../server/modles/user');
+
+var id = '58a69c4237006e70b0f185c6';
+
+// if (!ObjectId.isValid(id)) {
+//     console.log('Id not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+var userId = '58a3ecc6c234de549c0e3b25';
+
+if (!ObjectId.isValid(userId)) {
+    console.log('invalid User Id');
+}
+
+User.findById(userId).then((user) => {
+    if (!user) {
+        return console.log('User not found');
+    }
+
+    console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e));
