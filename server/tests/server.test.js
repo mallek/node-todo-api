fe6de0f1b14b32b88409e45d768ceed35d555803
mallek <travis@haleycomputersolutions.com>
2017-02-23 23:50:53 -0700

    test DELETE /user/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -302,3 +302,23 @@ describe('POST /users/login', function () {
             });
     });
 });
+
+describe('DELETE /users/me/token', function () {
+    it('should remove auth token on logout', function (done) {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .send()
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
